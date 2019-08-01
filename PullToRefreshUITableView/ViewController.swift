5e27edf70bf5d9b94a168eee204aad49aa0ebfab
Artem <inste@icloud.com>
2019-08-01 16:16:48 +0300

    First commit

--- PullToRefreshUITableView/ViewController.swift
+++ PullToRefreshUITableView/ViewController.swift
@@ -8,13 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var items = [String]()
+    
+    let myRefreshControl: UIRefreshControl = {
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refresh(sender:)), for: .valueChanged)
+        return refreshControl
+    }()
+
+    @IBOutlet weak var table: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        table.refreshControl = myRefreshControl
+
+    }
+    
+    @objc private func refresh(sender: UIRefreshControl) {
+        let string = "This is \(items.count) line"
+        items.append(string)
+        table.reloadData()
+        sender.endRefreshing()
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let currentString = items[indexPath.row]
+        cell.textLabel?.text = currentString
+        return cell
+    }
 
 }
 

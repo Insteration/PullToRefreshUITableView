c51f1f139389c5c9b9f054e50d88d2f5e24bfdbb
Artem <inste@icloud.com>
2019-08-01 16:08:50 +0300

    Initial Commit

---
+++ PullToRefreshUITableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PullToRefreshUITableView
+//
+//  Created by Артем Кармазь on 8/1/19.
+//  Copyright © 2019 Артём Кармазь. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
